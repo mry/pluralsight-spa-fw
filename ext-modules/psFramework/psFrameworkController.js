3a5397599631435455265ed7d221a3667ca34067
Mattias Rylander <mattias.rylander@gmail.com>
2016-01-07 00:39:27 +0100

    Clickable menus

--- ext-modules/psFramework/psFrameworkController.js
+++ ext-modules/psFramework/psFrameworkController.js
@@ -1,8 +1,12 @@
-"use strict";
+﻿"use strict";
 
 angular.module("psFramework").controller("psFrameworkController",
     ['$scope', 
         function ($scope) {
+
+            $scope.$on('ps-menu-item-selected-event', function (evt, data) {
+                $scope.routeString = data.route;
+            });
 
         }
     ]);
--- ext-modules/psFramework/psFrameworkDirective.js
+++ ext-modules/psFramework/psFrameworkDirective.js
@@ -1,10 +1,12 @@
-"use strict";
+﻿"use strict";
 
 angular.module("psFramework").directive("psFramework", function () {
     return {
-        transclude: false,
+        transclude: true,
         scope: {
-
+            title: '@',
+            subtitle: '@',
+            iconFile: '@'
         },
         controller: "psFrameworkController",
         templateUrl: "ext-modules/psFramework/psFrameworkTemplate.html"
