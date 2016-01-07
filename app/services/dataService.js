747408e1552da8f3669e1b4e5c60b0f58bb57329
Mattias Rylander <mattias.rylander@gmail.com>
2016-01-07 11:11:56 +0100

    Added ngStorage and functionality to save prefs

--- app/services/dataService.js
+++ app/services/dataService.js
@@ -126,7 +126,7 @@ angular.module('app').factory('dataService',
                 if (locations[i].id == id)
                     return locations[i];
             return undefined;
-        }, 2000);
+        }, 300);
 
         return timeout;
     };
